0b4b04b1f5aae9072777f7565c1f73bc3ad3b397
Michael112007 <117554393+Michael112007@users.noreply.github.com>
2024-02-12 08:42:46 -0400

    Agregar archivos de proyecto.

---
+++ registro_persona_gui01/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace registro_persona_gui01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.txt_edad = new System.Windows.Forms.TextBox();
+            this.btn_nuevo = new System.Windows.Forms.Button();
+            this.btn_guardar = new System.Windows.Forms.Button();
+            this.btn_editar = new System.Windows.Forms.Button();
+            this.btn_eliminar = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(158, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(158, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Edad";
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nombre.Location = new System.Drawing.Point(377, 94);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(296, 28);
+            this.txt_nombre.TabIndex = 2;
+            // 
+            // txt_edad
+            // 
+            this.txt_edad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_edad.Location = new System.Drawing.Point(377, 165);
+            this.txt_edad.Name = "txt_edad";
+            this.txt_edad.Size = new System.Drawing.Size(112, 28);
+            this.txt_edad.TabIndex = 3;
+            // 
+            // btn_nuevo
+            // 
+            this.btn_nuevo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_nuevo.Location = new System.Drawing.Point(134, 272);
+            this.btn_nuevo.Name = "btn_nuevo";
+            this.btn_nuevo.Size = new System.Drawing.Size(111, 59);
+            this.btn_nuevo.TabIndex = 4;
+            this.btn_nuevo.Text = "Nuevo";
+            this.btn_nuevo.UseVisualStyleBackColor = true;
+            this.btn_nuevo.Click += new System.EventHandler(this.btn_nuevo_Click);
+            // 
+            // btn_guardar
+            // 
+            this.btn_guardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_guardar.Location = new System.Drawing.Point(262, 272);
+            this.btn_guardar.Name = "btn_guardar";
+            this.btn_guardar.Size = new System.Drawing.Size(111, 59);
+            this.btn_guardar.TabIndex = 5;
+            this.btn_guardar.Text = "Guardar";
+            this.btn_guardar.UseVisualStyleBackColor = true;
+            this.btn_guardar.Click += new System.EventHandler(this.btn_guardar_Click);
+            // 
+            // btn_editar
+            // 
+            this.btn_editar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_editar.Location = new System.Drawing.Point(390, 272);
+            this.btn_editar.Name = "btn_editar";
+            this.btn_editar.Size = new System.Drawing.Size(111, 59);
+            this.btn_editar.TabIndex = 6;
+            this.btn_editar.Text = "Editar";
+            this.btn_editar.UseVisualStyleBackColor = true;
+            this.btn_editar.Click += new System.EventHandler(this.btn_editar_Click);
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_eliminar.Location = new System.Drawing.Point(528, 272);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Size = new System.Drawing.Size(111, 59);
+            this.btn_eliminar.TabIndex = 7;
+            this.btn_eliminar.Text = "Eliminar";
+            this.btn_eliminar.UseVisualStyleBackColor = true;
+            this.btn_eliminar.Click += new System.EventHandler(this.btn_eliminar_Click);
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salir.Location = new System.Drawing.Point(663, 272);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(111, 59);
+            this.btn_salir.TabIndex = 8;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 363);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_eliminar);
+            this.Controls.Add(this.btn_editar);
+            this.Controls.Add(this.btn_guardar);
+            this.Controls.Add(this.btn_nuevo);
+            this.Controls.Add(this.txt_edad);
+            this.Controls.Add(this.txt_nombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Registro de persona";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.TextBox txt_edad;
+        private System.Windows.Forms.Button btn_nuevo;
+        private System.Windows.Forms.Button btn_guardar;
+        private System.Windows.Forms.Button btn_editar;
+        private System.Windows.Forms.Button btn_eliminar;
+        private System.Windows.Forms.Button btn_salir;
+    }
+}
+
---
+++ registro_persona_gui01/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace registro_persona_gui01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_nuevo_Click(object sender, EventArgs e)
+        {
+            btn_nuevo.Enabled = false;
+            btn_guardar.Enabled = true;
+            btn_editar.Enabled = false;
+            btn_eliminar.Enabled = false;
+            txt_nombre.Clear();
+            txt_edad.Clear();
+
+
+            txt_nombre.Enabled = true;
+            txt_edad.Enabled = true;
+           
+            txt_nombre.Focus();
+
+        }
+
+        private void btn_guardar_Click(object sender, EventArgs e)
+        {
+            btn_nuevo.Enabled = true;
+            btn_guardar.Enabled = false;
+            btn_editar.Enabled = true;
+            btn_eliminar.Enabled = true;
+
+        }
+
+        private void btn_editar_Click(object sender, EventArgs e)
+        {
+            btn_nuevo.Enabled = false;
+            btn_guardar.Enabled = true;
+            btn_editar.Enabled = false;
+            btn_eliminar.Enabled = false;
+
+        }
+
+        private void btn_eliminar_Click(object sender, EventArgs e)
+        {
+            btn_nuevo.Enabled = true;
+            btn_guardar.Enabled = false;
+            btn_editar.Enabled = false;
+            btn_eliminar.Enabled = false;
+            txt_nombre.Clear();
+            txt_edad.Clear();
+
+        
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txt_nombre.Enabled=false;
+            txt_edad.Enabled=false;
+        }
+    }
+}
